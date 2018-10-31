44cec0efd9183c48867f1ce225d1b9ec504239c1
Sanjit Dutta <sdlynx@gmail.com>
2018-10-30 22:20:54 -0700

    Initial Commit

---
+++ OverdriveTests/OverdriveTests.swift
@@ -0,0 +1,34 @@
+//
+//  OverdriveTests.swift
+//  OverdriveTests
+//
+//  Created by Sanjit Dutta on 10/30/18.
+//  Copyright © 2018 Sanjit Dutta. All rights reserved.
+//
+
+import XCTest
+@testable import Overdrive
+
+class OverdriveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
